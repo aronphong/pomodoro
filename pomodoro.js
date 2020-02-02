24ff00b5430d82b7dff52bbe4b978948f1a32162
aron <aronphong@gmail.com>
2020-02-02 14:13:59 -0800

    bug fix timer

--- pomodoro.js
+++ pomodoro.js
@@ -1,9 +1,8 @@
 let pause = 0;
+let pausedTime = 0;
+let counter;
 
 function startTimer (duration, display) {
-
-    let counter;
-    clearInterval(counter);
 
     let timer = 0;
     let minutes = 0;
@@ -13,55 +12,47 @@ function startTimer (duration, display) {
         timer = duration*60;
     } 
     else {
-        timer = duration, minutes, seconds;
+        duration = pausedTime;
+        timer = duration;
+        counter = undefined;
     }
 
-    minutes = Number(pomodoroTime.textContent);
+    if (counter == undefined) {
+
+        counter = setInterval(() => {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
     
-    counter = setInterval(() => {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.innerText = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            clearInterval(counter);
-        }
-    }, 1000);
-
-    function pauseTimer() {
-        
-        pause = 1;
-
-        clearInterval(counter);
-        let pausedTime = timer;
-        console.log("pausedTime on pause = " + pausedTime)
-        display.innerText = minutes + ":" + seconds;
-
-        display.value = timer;
-
-        let resumeButton = document.querySelector("#pauseTimer");
-        resumeButton.innerText = "Resume";
-
-        resumeButton.addEventListener('click', () => {
-            console.log("pausedTime on click =" + pausedTime);
-            startTimer(pausedTime, document.querySelector("#displayCountDown"));
-            resumeButton.innerText = "Pause";
-            pause = 0;
-        });
-    }
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
     
-    function resetTimer() {
-        clearInterval(counter);
-        display.textContent = `${duration}:00`;
+            display.innerText = minutes + ":" + seconds;
+            console.log(seconds);
+            if (--timer < 0) {
+                clearInterval(counter);
+            }
+        }, 1000);
     }
-
-    startTimer.pauseTimer = pauseTimer;
-    startTimer.resetTimer = resetTimer;
 }
+
+function pauseTimer() {
+    clearInterval(counter);
+    pause = 1;
+    pausedTime = displayCountDown.innerText.split(":");
+    let minutes = Number(pausedTime[0]);
+    let seconds = Number(pausedTime[1]);
+    pausedTime = (minutes*60) + seconds;
+    return pause, pausedTime;
+}
+
+function resetTimer() {
+    clearInterval(counter);
+    counter = undefined;
+    pause = 0;
+    pausedTime = 0;
+    displayCountDown.textContent = `${pomodoroTime.textContent}:00`;
+}
+
 
 let pomodoroTime = document.querySelector("#focusTime");
 let displayCountDown = document.querySelector("#displayCountDown");
@@ -86,11 +77,11 @@ decreaseTime.addEventListener('click', () => {
 
 let resetButton = document.querySelector("#resetTimer");
 resetButton.addEventListener('click', () => {
-    startTimer.resetTimer();
+    resetTimer();
 });
 
 
 let pauseButton = document.querySelector("#pauseTimer");
 pauseButton.addEventListener('click', () => {
-    startTimer.pauseTimer();
+    pauseTimer();
 })
