aacb559a2205713cac3deeed83b5498f2bee5b5a
aron <aronphong@gmail.com>
2020-02-02 12:30:23 -0800

    Add Reset and Pause timer functions

--- pomodoro.js
+++ pomodoro.js
@@ -1,34 +1,66 @@
-function getTargetTime () {
+let pause = 0;
 
-    // 25 minutes
-    let targetTime = 25000;
-    return targetTime
-}
-
-let minutes = 25;
-let seconds = 0;
-
-// try to reuse for break time
 function startTimer (duration, display) {
 
-    let timer = duration*60, minutes, seconds;
+    let counter;
+    clearInterval(counter);
+
+    let timer = 0;
+    let minutes = 0;
+    let seconds = 0;
+
+    if (pause == 0) {
+        timer = duration*60;
+    } 
+    else {
+        timer = duration, minutes, seconds;
+    }
 
     minutes = Number(pomodoroTime.textContent);
     
-    let counter = setInterval(() => {
+    counter = setInterval(() => {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.innerText = minutes + ":" + seconds;
 
         if (--timer < 0) {
             clearInterval(counter);
         }
     }, 1000);
 
+    function pauseTimer() {
+        
+        pause = 1;
+
+        clearInterval(counter);
+        let pausedTime = timer;
+        console.log("pausedTime on pause = " + pausedTime)
+        display.innerText = minutes + ":" + seconds;
+
+        display.value = timer;
+
+        let resumeButton = document.querySelector("#pauseTimer");
+        resumeButton.innerText = "Resume";
+
+        resumeButton.addEventListener('click', () => {
+            console.log("pausedTime on click =" + pausedTime);
+            startTimer(pausedTime, document.querySelector("#displayCountDown"));
+            resumeButton.innerText = "Pause";
+            pause = 0;
+        });
+    }
+    
+    function resetTimer() {
+        clearInterval(counter);
+        display.textContent = `${duration}:00`;
+    }
+
+    startTimer.pauseTimer = pauseTimer;
+    startTimer.resetTimer = resetTimer;
 }
 
 let pomodoroTime = document.querySelector("#focusTime");
@@ -40,25 +72,25 @@ startButton.addEventListener('click', () => {
 })
 
 
-
 let increaseTime = document.querySelector("#increase");
 increaseTime.addEventListener('click', () => {
     pomodoroTime.textContent = Number(pomodoroTime.textContent) + 1;
 });
+
 
 let decreaseTime = document.querySelector("#decrease");
 decreaseTime.addEventListener('click', () => {
     pomodoroTime.textContent = Number(pomodoroTime.textContent) - 1;
 });
 
-// stop timer and reset to 25 minutes
+
 let resetButton = document.querySelector("#resetTimer");
-resetButton.addEventListener('click', resetTimer);
+resetButton.addEventListener('click', () => {
+    startTimer.resetTimer();
+});
 
 
-function resetTimer () {
-    minutes = 25;
-    seconds = 0;
-    clearInterval(counter);
-    console.log(counter);
-}
+let pauseButton = document.querySelector("#pauseTimer");
+pauseButton.addEventListener('click', () => {
+    startTimer.pauseTimer();
+})
