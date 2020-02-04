153ee6589523e3c215c02fc4115e3bbf8fea2c34
aron <aronphong@gmail.com>
2020-02-04 07:17:24 -0800

    Add break timer functionality

--- pomodoro.js
+++ pomodoro.js
@@ -1,13 +1,16 @@
 let pause = 0;
 let pausedTime = 0;
 let counter;
+let breakStatus = false;
 
 function startTimer (duration, display) {
 
     let timer = 0;
     let minutes = 0;
     let seconds = 0;
+    // let breakStatus = false;
 
+    // check pause status
     if (pause == 0) {
         timer = duration*60;
     } 
@@ -17,6 +20,13 @@ function startTimer (duration, display) {
         counter = undefined;
     }
 
+    if (breakStatus == true) {
+        console.log("yes");
+        timer = duration*60;
+        counter = undefined;
+    }
+
+    // start focus time
     if (counter == undefined) {
 
         counter = setInterval(() => {
@@ -27,9 +37,13 @@ function startTimer (duration, display) {
             seconds = seconds < 10 ? "0" + seconds : seconds;
     
             display.innerText = minutes + ":" + seconds;
-            console.log(seconds);
+
             if (--timer < 0) {
                 clearInterval(counter);
+                breakStatus = true;
+                console.log(breakStatus);
+                startTimer(breakTime.textContent,display);
+                return breakStatus
             }
         }, 1000);
     }
@@ -53,35 +67,42 @@ function resetTimer() {
     displayCountDown.textContent = `${pomodoroTime.textContent}:00`;
 }
 
+const pomodoroTime = document.querySelector("#focusTime");
+const breakTime = document.querySelector("#breakTime");
+const displayCountDown = document.querySelector("#displayCountDown");
 
-let pomodoroTime = document.querySelector("#focusTime");
-let displayCountDown = document.querySelector("#displayCountDown");
 
-let startButton = document.querySelector("#start");
+// event listeners for controls
+const startButton = document.querySelector("#startTimer");
 startButton.addEventListener('click', () => {
     startTimer(pomodoroTime.textContent, displayCountDown);
 })
 
+const resetButton = document.querySelector("#resetTimer");
+resetButton.addEventListener('click', resetTimer);
 
-let increaseTime = document.querySelector("#increase");
+const pauseButton = document.querySelector("#pauseTimer");
+pauseButton.addEventListener('click', pauseTimer);
+
+
+// Variables and functions to reduce and increase focus time
+const increaseTime = document.querySelector("#increase");
 increaseTime.addEventListener('click', () => {
     pomodoroTime.textContent = Number(pomodoroTime.textContent) + 1;
 });
 
-
-let decreaseTime = document.querySelector("#decrease");
+const decreaseTime = document.querySelector("#decrease");
 decreaseTime.addEventListener('click', () => {
     pomodoroTime.textContent = Number(pomodoroTime.textContent) - 1;
 });
 
-
-let resetButton = document.querySelector("#resetTimer");
-resetButton.addEventListener('click', () => {
-    resetTimer();
+// Variables and functions to reduce and increase break time
+const increaseBreak = document.querySelector("#increaseBreak");
+increaseBreak.addEventListener('click', () => {
+    breakTime.textContent = Number(breakTime.textContent) + 1;
 });
 
-
-let pauseButton = document.querySelector("#pauseTimer");
-pauseButton.addEventListener('click', () => {
-    pauseTimer();
-})
+const decreaseBreak = document.querySelector("#decreaseBreak");
+decreaseBreak.addEventListener('click', () => {
+    breakTime.textContent = Number(breakTime.textContent) - 1;
+});
